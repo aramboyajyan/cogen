124cb8d67ae8b6ab02dd866e912a4fcba8c5f8aa
Topsitemakers <support@topsitemakers.com>
2013-10-27 16:55:59 +0100

    fixes #1

--- cogen.py
+++ cogen.py
@@ -3,6 +3,24 @@ import os, sys, json, distutils.core
 
 config = json.load(open(os.path.join(os.path.dirname(__file__), 'configuration.json')))
 
+text_help = 'Help text'
+text_list = 'List text'
+text_about = 'About text'
+text_version = 'Cogen version: 1.0'
+text_default = 'Command not found. Type "cogen -h" to see help.'
+text_invalid_path = 'Template does not exist or "cogen.json" file is missing.'
+text_replacing = 'Starting to replace value: '
+text_path_not_found = 'Warning: path "%s" does not contain cogen.json file.'
+text_cogen_not_found = 'Warning: cogen.json file does not exist in "%s"'
+text_general_not_found = 'General variable "%s" not found.'
+text_generated = 'Template "%s" successfully generated.'
+
+args_out = ['-o', '--o', '--output']
+args_help = ['-h', '--h', '--help']
+args_list = ['-l', '--l', '--list']
+args_check = ['-c', '--c', '--check']
+args_version = ['-v', '--v', '--version']
+
 argument = sys.argv[1]
 
 def replace_file_contents(destination, variable, value):
@@ -24,20 +42,20 @@ def replace_file_names(destination, variable, value):
         os.rename(path + '/' + filename, path + '/' + filename.replace(variable['pattern'], value))
 
 if argument[0] == '-':
-  if argument == '-h':
-    print 'Help text'
-  elif argument == '-v':
-    print 'Cogen version 1.0.0'
-  elif argument == '-l':
-    print 'List text'
+  if argument in args_help:
+    print text_help
+  elif argument in args_version:
+    print text_version
+  elif argument in args_list:
+    print text_list
   else:
-    print 'Command not found. Type "python cogen.py -h" to see help.'
+    print text_general_not_found
 else:
   for path_to_template in config.get('paths_to_templates'):
     if not os.path.exists(path_to_template + '/' + argument):
-      print 'path does not exist: ' + path_to_template
+      print text_template_not_found % path_to_template
     elif not os.path.isfile(path_to_template + '/cogen.json'):
-      print 'cogen.json not found'
+      print text_cogen_not_found % path_to_template
     else:
       project_config = json.load(open(path_to_template + '/' + argument + '/cogen.json'))
       output_directory_name = raw_input('Enter folder name')
@@ -54,3 +72,4 @@ else:
             replace_file_contents(destination, variable, value)
           elif variable['type'] == 'rename':
             replace_file_names(destination, variable, value)
+      print text_generated % project_config['name']
